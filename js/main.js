a06b7b6785b9598b15158b8a9a59ce25c95f7b85
karenxiong <karen.xiong@mail.utoronto.ca>
2016-04-09 02:10:19 -0400

    Changes:
    - fixed dropdown menu to dropdown according to parent list
    - *needs to fix styling

--- js/main.js
+++ js/main.js
@@ -1,24 +1,17 @@
-
-  function myFunction() {
-      document.getElementById("myDropdown").classList.toggle("show");
-  }
-
-  // Close the dropdown menu if the user clicks outside of it
+  // Close any open dropdown menu, when user clicks outside of it
   window.onclick = function(event) {
     if (!event.target.matches('.dropbtn')) {
-
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
+      $(".dropdown-content").hide();
     }
   }
 
 $(document).ready(function(){
+  // when a menu bar button is clicked, the dropdown will show
+  $(".dropbtn").click(function() {
+    $(".dropdown-content").hide(); // hide all other menus first
+    $(this).children(".dropdown-content").show();
+  });
+
   /* When the user clicks on the button, 
   toggle between hiding and showing the dropdown content */
   function updateTime() {
