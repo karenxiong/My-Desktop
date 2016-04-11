fe990ceaa80632ada84fc8cbb2177c2361cd3e1e
karenxiong <karen.xiong@mail.utoronto.ca>
2016-04-11 00:09:35 -0400

    Changes:
    - Got snake application to show on double clicking through the icon
    - deleted draggable css + js
    - changed the size of snake
    - changed the location of snake in the DOM

--- js/main.js
+++ js/main.js
@@ -10,8 +10,42 @@ $(document).ready(function(){
   $(".dropbtn").click(function() {
     $(".dropdown-content").hide(); // hide all other menus first
     $(this).children(".dropdown-content").show();
-    $(this).toggleClass('active');
   });
+
+  // Enternet to show on doubleclick through app
+
+  $(".enternet").dblclick(function() {
+    $("#enternet").show();
+  });
+
+  // Enternet to close and minimize through buttons
+
+  $(".close, .minimize").click(function() {
+    $("#enternet").hide();
+  });
+
+  // Snake to close and minimize through buttons
+
+  $(".sclose, .sminimize").click(function() {
+    $("#snake").css("visibility", "hidden");
+  });
+
+  // Snake opening on doubleclick through snake app
+
+  $(".snake").dblclick(function() {
+    // $("#snake").show();
+    if ($("#snake").css("visibility") == "hidden")
+      $("#snake").css('visibility', 'visible');
+    else
+      $("#snake").css("visibility", "hidden");
+
+  });
+
+  // $(function() {
+  //   $(".etop").draggable();
+  // });
+
+
 
   /* When the user clicks on the button, 
   toggle between hiding and showing the dropdown content */
