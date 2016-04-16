66c7ed6bc4f09aab9f2b6fdc67fb670209a57b8a
karenxiong <karen.xiong@mail.utoronto.ca>
2016-04-16 19:39:55 -0400

    Added features:
    - working overlapping windows
    - refactored and improved css, javascript and html

--- js/main.js
+++ js/main.js
@@ -6,39 +6,42 @@
   }
 
 $(document).ready(function(){
+  var maxIndex = 0; // used to decide which window overlaps another
+
   // when a menu bar button is clicked, the dropdown will show
   $(".dropbtn").click(function() {
     $(".dropdown-content").hide(); // hide all other menus first
     $(this).children(".dropdown-content").show();
   });
 
-  // Enternet to show on doubleclick through app
-
-  $(".enternet").dblclick(function() {
-    $("#enternet").show();
-  });
+  /* ----App button behaviors ----*/
+  // double click to show app
+  $(".apps div").dblclick(function() {
+    var $appWindow = $("#" + $(this).attr("class"));
+    maxIndex += 1;
+    $appWindow.css("z-index", maxIndex);
+    $appWindow.show();
+  })
 
   // Enternet to close and minimize through buttons
-
   $(".close, .minimize").click(function() {
     $("#enternet").hide();
   });
 
   // Snake to close and minimize through buttons
-
   $(".sclose, .sminimize").click(function() {
     $("#snake").css("visibility", "hidden");
   });
 
   // Snake opening on doubleclick through snake app
-
   $(".snake").dblclick(function() {
+    maxIndex += 1;
+    $("#snake").css("z-index", maxIndex);
     // $("#snake").show();
     if ($("#snake").css("visibility") == "hidden")
       $("#snake").css('visibility', 'visible');
     else
       $("#snake").css("visibility", "hidden");
-
   });
 
   // $(function() {
